f8f516eef247f7192465fd69356387784d8203b3
ajinkyabridgelabz <ajinkya.kambe@bridgelabz.com>
2023-09-15 16:30:46 +0530

    Added the basic Spring structure

---
+++ src/test/java/com/bridgelabz/bloombox/BloomboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.bloombox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloomboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
